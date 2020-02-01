bd6f3bfb7216270e9440d2f70a657226d4845074
Darren Rose <thorn086@gmail.com>
2020-01-31 22:29:52 -0600

    first commit

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
+const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV} = require('./config')
-const errorHandler = require('./error-handler')
+const { NODE_ENV } = require('./config')
+
 const app = express()
-const foldersRouter = require('./folders/folders-router')
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-const notesRouter = require('./notes/notes-router')
+
+const morganOption = (NODE_ENV === 'production' 
+  ? 'tiny'
+  : 'common')
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.use(errorHandler)
-app.get('/', (req, res) =>{
-    res.send("Hello, world!")
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
-app.use('/api/folders', foldersRouter)
-app.use('/api/notes', notesRouter)
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if(NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
 
 module.exports = app
